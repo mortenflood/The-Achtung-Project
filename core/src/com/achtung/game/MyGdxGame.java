19733448563b88125036878eb8f152a1a7d1f051
mortenflood <mortenflood@hotmail.com>
2016-03-03 16:07:33 -0800

    Moved multiplayer files

--- core/src/com/achtung/game/MyGdxGame.java
+++ core/src/com/achtung/game/MyGdxGame.java
@@ -5,17 +5,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class MyGdxGame extends ApplicationAdapter {
 	//SpriteBatch batch;
 	//Texture img;
 	public static int WIDTH;
 	public static int HEIGHT;
+	Stage stage;
+	TextButton button;
+
 	
 	@Override
 	public void create () {
 		//batch = new SpriteBatch();
 		//img = new Texture("badlogic.jpg");
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
 
 
 	}
--- core/src/com/achtung/game/SplashScreen.java
+++ core/src/com/achtung/game/SplashScreen.java
@@ -22,7 +22,7 @@ public class SplashScreen implements Screen {
 
     @Override
     public void show() {
-        
+
 
     }
 
