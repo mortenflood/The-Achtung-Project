ee225fb0e68d65d8f292ccf13fedc55d0ab53206
mortenflood <mortenflood@hotmail.com>
2016-03-04 16:00:54 -0800

    Appwarp keys added

---
+++ core/src/com/achtung/game/Constants.java
@@ -0,0 +1,10 @@
+package com.achtung.game;
+
+/**
+ * Created by mortenflood on 03.03.16.
+ */
+public class Constants {
+    public static String apiKey = "dedfcbf1d40ed332c0db392a53f487c6d6a07e18ef0ff24813af89fd3c17641f";
+    public static String secretKey = "22610460c58c8a198c42d2e2acbbb8e440a7d5a681dffe8feb0b3e8c9f62867a";
+
+}
